26eeb0a219cacf40212fc8213a2a55337f444ed8
emludei <incontrol64@yandex.ru>
2019-06-10 23:48:33 +0300

    add several log adapters from pqx

---
+++ log/zerologadapter/adapter.go
@@ -0,0 +1,36 @@
+// Package zerologadapter provides a logger that writes to a github.com/rs/zerolog.
+package zerologadapter
+
+import (
+	gs "github.com/emludei/graceful-shutdown"
+	"github.com/rs/zerolog"
+)
+
+type Logger struct {
+	logger zerolog.Logger
+}
+
+func (l *Logger) Log(level gs.LogLevel, msg string) {
+	var zlevel zerolog.Level
+
+	switch level {
+	case gs.LogLevelDebug:
+		zlevel = zerolog.DebugLevel
+	case gs.LogLevelInfo:
+		zlevel = zerolog.InfoLevel
+	case gs.LogLevelError:
+		zlevel = zerolog.ErrorLevel
+	default:
+		zlevel = zerolog.ErrorLevel
+	}
+
+	l.logger.WithLevel(zlevel).Msg(msg)
+}
+
+// NewLogger accepts a zerolog.Logger as input and returns a new custom pgx
+// logging fascade as output.
+func NewLogger(logger zerolog.Logger) *Logger {
+	return &Logger{
+		logger: logger.With().Str("module", "graceful-shutdown").Logger(),
+	}
+}
